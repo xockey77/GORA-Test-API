788583baa318c7a83e611231cdb4d064b7174ccd
Andrey Belov <belov_andrey@hotmail.com>
2022-01-18 20:05:14 +0500

    Initial Commit

---
+++ GORA Test API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GORA Test API
+//
+//  Created by username on 18.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
